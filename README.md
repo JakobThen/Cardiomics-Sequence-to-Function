269e8b3d2410021461659a5ab293ab49e13c6738
Jakob Then <jakobmoritzthen@gmail.com>
2026-05-28 23:27:34 +0200

    docstrings

---
+++ README.md
@@ -0,0 +1,6 @@
+# Cardiomics-Sequence-to-Function Documentation
+
+Collection of Tools and pipelines to train and finetune sequence to dunction models on pseudobulked scMultiome and bulk CutnTag data.
+
+Documentation:
+https://cardiomics-sequence-to-function.readthedocs.io/en/latest/index.html
---
+++ src/utils/preprocessing/__init__.py
@@ -0,0 +1,13 @@
+"""
+Preprocessing Package
+=====================
+
+This package contains scripts and utilities for preparing raw sequencing data 
+(like ATAC and RNA) into the specific input formats required by AlphaGenome 
+and Borzoi models. 
+
+It handles:
+* BigWig generation
+* Cell counting and metadata extraction
+* Final configuration `.yaml` construction
+"""
